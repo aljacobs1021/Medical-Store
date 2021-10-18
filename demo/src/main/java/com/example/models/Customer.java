ae2413279432fcfc5530f360b69855f961cd0b00
aljacobs1021 <aljacobs1021@gmail.com>
2021-10-18 11:12:51 -0500

    woops

--- demo/src/main/java/com/example/models/Customer.java
+++ demo/src/main/java/com/example/models/Customer.java
@@ -7,8 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+//import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 //import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,7 +17,7 @@ import lombok.ToString;
 @Getter
 @Setter
 
-@AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Entity
 @Table
