3c4923339e302a19063681bed78063dc2cb1d847
AudreyKK <aukkeefe@gmail.com>
2021-10-18 13:42:25 -0400

    added some fields to product and @ManyToOne annotation

--- demo/src/main/java/com/example/demo/models/Product.java
+++ demo/src/main/java/com/example/demo/models/Product.java
@@ -2,9 +2,12 @@ package com.example.demo.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -32,11 +35,21 @@ public class Product {
 	@Column (name="prod_description", nullable = false)
 	private String description;
 	
-	@Column (name="supplier_id", nullable = false)
-	private int supplier;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="sup_id", nullable = false)
+	private Supplier supplier;
 	
 	@Column (name="price", nullable = false)
 	private double pricePerUnit;
+	
+	@Column(name="current_stock", nullable=false)
+	private int currentStock;
+	
+	@Column(name="min_limit", nullable=false)
+	private int minLimit;
+	
+	@Column(name="total_sold", nullable=false)
+	private double totalSold;
 
 	public Product(String name, String description, double pPU) {
 		super();
