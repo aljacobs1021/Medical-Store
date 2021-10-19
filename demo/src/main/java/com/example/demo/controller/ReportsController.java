4f90eefa41f6d440843a7c2b7cbc9ecacdbb84f2
aljacobs1021 <aljacobs1021@gmail.com>
2021-10-18 21:47:48 -0500

    Current Reports - no red

--- demo/src/main/java/com/example/demo/controller/ReportsController.java
+++ demo/src/main/java/com/example/demo/controller/ReportsController.java
@@ -16,19 +16,21 @@ import com.example.demo.dao.ReportsDAO;
 import com.example.demo.models.Customer;
 import com.example.demo.models.Reports;
 import com.example.demo.service.CustomerManager;
+import com.example.demo.service.ReportsService;
 
 @RestController
 @RequestMapping(path="reports")
 public class ReportsController {
 	
 	private static final Logger logger = LogManager.getLogger(ReportsController.class);
-
+	private ReportsService rServ;
+	
 	
 	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping(path="/getAll", produces="application/json")
 	public ResponseEntity<List<Reports>> getAll(){
 		logger.info("beep");
-		List<Reports> reportList = ReportsDAO.findAll();
+		List<Reports> reportList = rServ.findAll();
 		logger.info("beep1");
 		return new ResponseEntity<List<Reports>>(reportList, HttpStatus.OK);
 	}
