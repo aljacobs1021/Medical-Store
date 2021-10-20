bd0709246cd886ca4ad11487856da8527b78efc3
AudreyKK <aukkeefe@gmail.com>
2021-10-20 12:31:07 -0400

    fix products/invoice-download invoice as pdf

--- front-end/src/app/invoice-details/invoice-details.component.ts
+++ front-end/src/app/invoice-details/invoice-details.component.ts
@@ -3,7 +3,8 @@ import { Transaction } from '../interfaces/transaction';
 import { TransactionService } from '../services/transaction.service';
 import { Customer } from '../interfaces/customer';
 import { CustomerService } from '../services/customer.service';
-
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoice-details',
@@ -15,6 +16,7 @@ export class InvoiceDetailsComponent implements OnInit {
   
 
   filteredTransactions: Transaction[] = []
+  total: number = 0
 
   constructor(private transactionService: TransactionService, 
               private customerService: CustomerService) {
@@ -45,7 +47,35 @@ export class InvoiceDetailsComponent implements OnInit {
     this.transactionService.findByName(id)
       .subscribe(transactions => {
         this.filteredTransactions = transactions
+        this.findSum();
       })
   }
+
+  download(){
+    var element = document.getElementById('pdfTable')!;
+    html2canvas(element).then((canvas) => {
+      console.log(canvas);
+      var imgData = canvas.toDataURL('image/png')
+      var doc = new jsPDF();
+
+      var imgHeight = canvas.height * 180 / canvas.width;
+
+      doc.addImage(imgData, 0, 0, 208, imgHeight)
+
+      doc.save("image.pdf");
+    })
+  }
+
+  findSum() {
+    for(let i = 0; i < this.filteredTransactions.length; i++) {
+      this.total += (this.filteredTransactions[i].product.pricePerUnit * this.filteredTransactions[i].quantity);
+    }
+    console.log(this.total)
+  }
+
+  setZero() {
+    this.total = 0;
+  }
+
 
 }
